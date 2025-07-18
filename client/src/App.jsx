44bc3ddb48c0f4b87c487c3cf5af6b3506c749b6
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-07-18 20:27:05 +0545

    Add NotFound component and update routing to handle 404 errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,17 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import EmailVerify from "./pages/EmailVerify";
 import ResetPassword from "./pages/ResetPassword";
-
+import NotFound from "./pages/404";
 
 const App = () => {
   return (
     <div>
-    
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/email-verify" element={<EmailVerify />}></Route>
         <Route path="/reset-password" element={<ResetPassword />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
